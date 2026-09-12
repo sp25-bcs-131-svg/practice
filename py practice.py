92940a174b318bcc61abea42bd300f336cc1ff62
Talha Munir <SP25-BCS-131@cuilahore.edu.pk>
2026-09-12 15:37:29 -0700

    Practice

---
+++ py practice.py
@@ -0,0 +1,29 @@
+print("Hello , World")
+print(23)
+print(40+60)
+
+name = "Talha"
+age = 21
+old = False
+print("My name is",name,"and age is",age,"and it is",old,"that i am old")
+
+price = 500.00
+
+print(type(name))
+print(type(age))
+print(type(price))
+
+a = 10
+b = 20
+sum = a + b
+diff = a - b
+product = a * b
+div = b / a
+exp = a ** b
+mod = b % a
+print(sum)
+print(diff)
+print(product)
+print(div)
+print(exp)
+print(mod)
