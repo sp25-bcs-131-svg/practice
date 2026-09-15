1af6786443196158b078419029f14aad066e34a9
Talha Munir <SP25-BCS-131@cuilahore.edu.pk>
2026-09-15 10:24:24 -0700

    changes 8

--- py practice.py
+++ py practice.py
@@ -87,4 +87,20 @@ print(num1 < num2)
 print(num1 >= num2)
 print(num1 <= num2)
 
+#Assignment Operators
+num = 10
+num += 5
+print(num)
+num -= 5
+print(num)
+num *= 2
+print(num)
+num /= 2
+print(num)
+num **= 2
+print(num)
+num %= 3
+print(num)
+num //= 2
+print(num)
 
