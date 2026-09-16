6878979b21930d60338ebcb15d8efcea8945a2e6
Talha Munir <SP25-BCS-131@cuilahore.edu.pk>
2026-09-16 13:27:14 -0700

    changes 13

--- practice 2.py
+++ practice 2.py
@@ -4,3 +4,17 @@ str1 = "Hello My name is Talha,\n whats yours"
 str2 = "Hello My name is Talha,\t whats yours"
 print(str1)
 print(str2)
+
+name1 = "Talha"
+name2 = "Munir"
+full = name1 + " " + name2
+print(full)
+print(len(full))
+print(full[0])
+print(full[0:5])
+print(full[6:])
+print(full[-5:-1])
+print(full[:8])
+
+
+
