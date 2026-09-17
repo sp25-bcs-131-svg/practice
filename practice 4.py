71eb5bb1acfcfadaa5e1ea3b8cc236b5a025c90e
Talha Munir <SP25-BCS-131@cuilahore.edu.pk>
2026-09-17 23:18:43 +0500

    changes 21

--- practice 4.py
+++ practice 4.py
@@ -12,3 +12,26 @@ print(dict["name"])
 print(dict["language"])
 dict["age"] = 21
 print(dict)
+
+null_dict = {}
+null_dict["name"] = "Talha"
+print(null_dict)
+
+dict1 = {
+    "name": "Talha",
+    "age": 20,
+    "dict2": {
+        "University": "CUI",
+        "Department": "CS",
+        "CGPA": 3.5,
+        "Semester": 4
+    }
+}
+print(dict1)
+print(dict1["dict2"]["CGPA"])
+
+print(dict.keys())
+print(dict.values())
+print(dict.items())
+print(dict.get("name"))
+print(dict.update({"name": "Ali"}))
