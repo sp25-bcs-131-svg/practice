65d8f197627591481dd35954c5d919820ec8c55b
Talha Munir <SP25-BCS-131@cuilahore.edu.pk>
2026-09-17 22:47:45 +0500

    changes 19

--- practice 3.py
+++ practice 3.py
@@ -31,3 +31,12 @@ print(len(tuple))
 print(tuple[0:3])
 print(tuple.index(3))
 print(tuple.count(2))
+
+list1 = [1, 2, 3, 2, 1]
+list2 = list1.copy()
+list2.reverse
+if(list1 == list2):
+    print("Palindrome")
+else:
+    print("Not Palindrome")
+
