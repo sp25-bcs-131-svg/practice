73a827ec07b3bb976f4deaa5a1a8c1fa77cf6f6a
Talha Munir <SP25-BCS-131@cuilahore.edu.pk>
2026-09-17 23:07:57 +0500

    changes 20

---
+++ practice 4.py
@@ -0,0 +1,14 @@
+#Dictionarys
+dict = {
+    "name": "Talha",
+    "age": 20,
+    "city": "Lahore",
+    "weight": 70.67,
+    "language": ["Python", "JavaScript"],
+    "IsAdult": True
+}
+print(dict)
+print(dict["name"])
+print(dict["language"])
+dict["age"] = 21
+print(dict)
