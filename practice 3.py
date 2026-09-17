59c3ff54b8af9c6f3bf6e9951e266551ba0c97cc
Talha Munir <SP25-BCS-131@cuilahore.edu.pk>
2026-09-17 22:35:16 +0500

    changes 17

--- practice 3.py
+++ practice 3.py
@@ -4,8 +4,17 @@ print(name)
 print(name[0])
 print(len(name))
 print(name[0:3])
-print(name[-1:-2])
+print(name[-3:-1])
 print(name[:4])
 
-
-
+list = [2,1,3,5,4]
+list.append(6)
+print(list)
+list.sort()
+print(list)
+list.sort(reverse=True)
+print(list)
+list.reverse()
+print(list)
+list.insert(2,7)
+print(list)
